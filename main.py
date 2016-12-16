7ca725272ce6041a31fd82c0206a033739865913
Vladimir Kuznetsov <vokuznecov@gmail.com>
2016-12-17 00:26:03 +0300

    some update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import ScatterSearch as ss
 
 
 def main():
-    graphs_name = ["small_p100.txt", "hardwell_will199.txt", "hardwell_fs680.txt", "hardwell_dwt592.txt",
-                   "hardwell_west0655.txt"]
+    graphs_name = ["small_p100.txt", "hardwell_will199.txt", "hardwell_dwt592.txt",
+                   "hardwell_fs680.txt", "hardwell_west0655.txt"]
 
-    for i in range(0, len(graphs_name)):
+    for i in range(0, 3):
         print 'FILE NAME: ' + graphs_name[i] + '\n'
         ss.scatter_search(graphs_name[i])
         print "-----------------------"
